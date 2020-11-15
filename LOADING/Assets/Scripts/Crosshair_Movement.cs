ea112c2fb57ce60b1a13715ee211cd604ad2feb3
szejkerek <bartekk.gordon@gmail.com>
2020-11-15 16:47:09 +0100

    PlayerMovement, Crosshair

---
+++ LOADING/Assets/Scripts/Crosshair_Movement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair_Movement : MonoBehaviour
+{
+    private SpriteRenderer sp;
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        sp.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = vec;
+    }
+}
---
+++ LOADING/Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigitBody;
+    private Animator anim;
+    private Vector3 change;
+    void Start()
+    {
+        myRigitBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            anim.SetFloat("moveX", change.x);
+            anim.SetFloat("moveY", change.y);
+        }
+     
+    }
+
+    void MoveCharacter()
+    {
+        myRigitBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+
+
+}
